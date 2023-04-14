0ded8eee55d49979121ac280d9d78eaf02b4f967
Максим Долженко <maxim_2003@mail.ru>
2023-04-14 23:42:26 +0300

    Flask и его маршрутизация

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
+from app.forms import MainForm
+from app.service import comparator
 
 
-@app.route('/')
-@app.route('/index')
+class User:
+    def __init__(self, username):
+        self.username = username
+
+
+user = User('Maxim')
+matched = False
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Привет, Мир!\n'
+    global matched
+    form = MainForm()
+    if form.validate_on_submit():
+        source = form.raw_data.data
+        regular_ex = form.regular_expression.data
+        matched = comparator.RegularExpressionComparator.equals_source_and_regular_ex(source, regular_ex)
+        print(matched)
+        return render_template('index.html', form=form, matched=matched)
+    return render_template('index.html', form=form)
